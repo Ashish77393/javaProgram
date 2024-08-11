9dc95bf8097bd6fa68ff6f87b5e007d0618c7f42
Ashish77393 <ashishkumar77393@gmail.com>
2024-08-11 16:19:41 +0530

    java programs

---
+++ HeapClass.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class HeapClass {
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data) {
+            arr.add(data);
+            int x = arr.size() - 1;
+            int par = (x - 1) / 2;
+            while (arr.get(x) > arr.get(par)) {
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+                x = par;
+                par = (x - 1) / 2;
+            }
+        }
+
+        public int peek() {
+            return arr.get(0);
+        }
+
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+            if (minIdx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+                heapify(minIdx);
+            }
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+            // step 1
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+            // step 2 delete node
+            arr.remove(arr.size() - 1);
+            // step 3 Heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Heap s = new Heap();
+        s.add(2);
+        s.add(5);
+        s.add(1);
+        s.add(5);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.remove();
+        }
+    }
+}
